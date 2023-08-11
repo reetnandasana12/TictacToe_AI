5c004f871cf55f7cf21d30b303e8b09149f1f070
Reet Nandasana <68194074+reetnandasana12@users.noreply.github.com>
2023-08-11 18:06:35 +0530

    Tictactoe version 1.2

--- app/src/main/java/com/example/tictactoe_ai/SplashScreenActivity.java
+++ app/src/main/java/com/example/tictactoe_ai/SplashScreenActivity.java
@@ -1,24 +1,61 @@
 package com.example.tictactoe_ai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference("Version");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_activity);
 
-        new Handler().postDelayed(new Runnable() {
+        databaseReference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this,MainActivity2.class));
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    if (task.getResult().exists()) {
+                        Log.d("Multiplayer", "arrange: error");
+
+                        DataSnapshot dataSnapshot = task.getResult();
+
+                        Boolean count = (Boolean) dataSnapshot.child("1.1").getValue();
+
+                        if (count){
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    startActivity(new Intent(SplashScreenActivity.this,MainActivity2.class));
+                                }
+                            },10);
+                        }
+                        else {
+                            Toast.makeText(SplashScreenActivity.this, "New Version is available Update", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }
             }
-        },10);
+        });
+
+
+
     }
 
 
